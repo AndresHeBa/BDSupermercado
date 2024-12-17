32a9ca668bc60a795ba8b9406b78cf6563c6af04
Andres Heredia <herediaandres040@gmail.com>
2024-12-16 19:31:05 -0600

    Bajas

---
+++ bajasEmpleados.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'mySQLConnection.php';
+        if (isset($_POST['idEmp'])) {
+            $sql = "DELETE FROM empleado WHERE CodigoEmpleado = " . $_POST["idEmp"] . ";";
+            $result = $conn->query($sql);
+            echo "Empleado borrado.";
+        }
+    ?>
+    <body>
+    <h4>Borrar Empleado</h4>
+    <form method="POST">
+        <label for="idEmp">ID del empleado a borrar:</label><br>
+        <input type="text" id="idEmp" name="idEmp"><br>
+        <input type="submit" value="Borrar empleado">
+    </form> 
+    </body>
+</body>
+</html>
---
+++ bajasVenta.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include 'mySQLConnection.php';
+        if (isset($_POST['idVen'])) {
+            $sql = "DELETE FROM venta WHERE CodigoVenta = " . $_POST["idVen"] . ";";
+            $result = $conn->query($sql);
+            echo "Venta borrada.";
+        }
+    ?>
+    <body>
+    <h4>Borrar Articulo</h4>
+    <form method="POST">
+        <label for="idEmp">ID de la venta a borrar:</label><br>
+        <input type="text" id="idEmp" name="idVen"><br>
+        <input type="submit" value="Borrar venta">
+    </form> 
+    </body>
+</body>
+</html>
